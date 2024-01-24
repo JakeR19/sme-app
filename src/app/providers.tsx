0d0dd1e110f2ec56aedf2ffe829ee504c0828324
JakeR19 <jake.reeve00@gmail.com>
2024-01-24 14:24:35 +0000

    setting up db models in prisma

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,6 +2,7 @@
 
 import { SessionProvider } from "next-auth/react";
 
+// providers need to be on client side so we make separate file here
 export default function Providers({ children }: { children: React.ReactNode }) {
   return <SessionProvider>{children}</SessionProvider>;
 }
