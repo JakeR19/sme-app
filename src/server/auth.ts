b14aca8c36f7407463054c28df25949305c75c1f
JakeR19 <jake.reeve00@gmail.com>
2024-03-07 22:15:28 +0000

    migrating

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,24 +1,41 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import {
   getServerSession,
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
+import { type Adapter } from "next-auth/adapters";
 import GithubProvider from "next-auth/providers/github";
 
 import { env } from "~/env";
 import { db } from "~/server/db";
 
-// next auth boilerplate
+/**
+ * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
+ * object and keep type safety.
+ *
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ */
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
       id: string;
+      // ...other properties
+      // role: UserRole;
     } & DefaultSession["user"];
   }
+
+  // interface User {
+  //   // ...other properties
+  //   // role: UserRole;
+  // }
 }
 
-// next auth boilerplate
+/**
+ * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
+ *
+ * @see https://next-auth.js.org/configuration/options
+ */
 export const authOptions: NextAuthOptions = {
   callbacks: {
     session: ({ session, user }) => ({
@@ -29,11 +46,7 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   },
-  pages: {
-    signIn: "/",
-  },
-  adapter: PrismaAdapter(db),
-  //auth providers
+  adapter: PrismaAdapter(db) as Adapter,
   providers: [
     GithubProvider({
       clientId: env.GITHUB_ID,
@@ -42,5 +55,9 @@ export const authOptions: NextAuthOptions = {
   ],
 };
 
-//gets auth session from the server
+/**
+ * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
+ *
+ * @see https://next-auth.js.org/configuration/nextjs
+ */
 export const getServerAuthSession = () => getServerSession(authOptions);
